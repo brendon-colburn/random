98fa3da4c9b8b96339a20cc75addcb58ca9dfa82
Brendon <brendon.colburn@gmail.com>
2023-06-01 22:11:26 -0400

    final formatting and commenting for blogsanity

--- pvaAdaptiveCardAdvanced.js
+++ pvaAdaptiveCardAdvanced.js
@@ -1,58 +1,60 @@
+// note that the assignment of pvaAdvancedAdaptiveCard variable is just done for documentation purposes, it is not used in the code
 pvaAdvancedAdaptiveCard = {
-    type: "AdaptiveCard",
-    '$schema': "http://adaptivecards.io/schemas/adaptive-card.json",
-    version: "1.3",
-    body: [
-      {
-        type: "TextBlock",
-        text: Topic.ResultObject.description, // "Note that you always need to specify Topic as the root object"
-        size: "Medium",
-        weight: "Bolder",
-        wrap: true,
-        separator: true
-      },
-      {
-        type: "FactSet",
-        facts: 
-          ForAll(Topic.ResultObject.imagetags, // So this is what was different, we used Power FX to return the facts for the factset
+  type: "AdaptiveCard",
+  '$schema': "http://adaptivecards.io/schemas/adaptive-card.json",
+  version: "1.3",
+  body: [
+    {
+      type: "TextBlock",
+      text: Topic.ResultObject.description, // "Note that you always need to specify Topic as the root object"
+      size: "Medium",
+      weight: "Bolder",
+      wrap: true,
+      separator: true
+    },
+    {
+      type: "FactSet",
+      facts:
+        ForAll(Topic.ResultObject.imagetags, // So this is what was different, we used Power FX to return the facts for the factset
           {
             title: ThisRecord.id,
             value: ThisRecord.tag
           })
-        ,
-        separator: true,
-        id: "id"
-      }
-    ]
-  }
+      ,
+      separator: true,
+      id: "id"
+    }
+  ]
+}
 
-  // this is the old way of doing it, which didn't seem to work any longer:
+// This was also written as oldWay = JSON for formatting purposes, but it is not used in the code
+// this is the old way of doing it, which didn't seem to work any longer:
 
- oldWay = Json({
-    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-    "type": "AdaptiveCard",
-    "version": "1.3",
-    "body": [
+oldWay = Json({
+  "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+  "type": "AdaptiveCard",
+  "version": "1.3",
+  "body": [
+    {
+      "type": "TextBlock",
+      "text": "${description}",
+      "size": "Medium",
+      "weight": "Bolder",
+      "wrap": true,
+      "separator": true
+    },
+    {
+      "type": "FactSet",
+      "facts": [
         {
-            "type": "TextBlock",
-            "text": "${description}",
-            "size": "Medium",
-            "weight": "Bolder",
-            "wrap": true,
-            "separator": true
-        },
-        {
-            "type": "FactSet",
-            "facts": [
-                {
-                    "$data": "${imagetags}",
-                    "title": "${id}",
-                    "value": "${tag}"
-                }
-            ],
-            "separator": true,
-            "$data": "${$root['imagetags[0]']}",
-            "id": "id"
+          "$data": "${imagetags}",
+          "title": "${id}",
+          "value": "${tag}"
         }
-    ]
+      ],
+      "separator": true,
+      "$data": "${$root['imagetags[0]']}",
+      "id": "id"
+    }
+  ]
 });
